308570b69f5cf0b8ea26736e0c03bffd67743006
Alex Roubinet <alex.roubinet@gmail.com>
2016-11-14 22:42:29 +0100

    modification template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
   end
+
+  def team
+    @members = ["Alex","Mourad", "Antoine"]
+  end
+
+  def join_us
+    render plain: "Apply now"
+  end
+
 end
