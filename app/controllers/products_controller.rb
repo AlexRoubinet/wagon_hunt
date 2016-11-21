b3e83d2eed24d4103989d6faef542ecd25e23262
Alex Roubinet <alex.roubinet@gmail.com>
2016-11-21 13:04:03 +0100

    Ajout des actions ajout/suppr produit / refacto du code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
 
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
       @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -14,7 +15,6 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(product_params)
     if @product.save
       redirect_to products_path
     else
@@ -23,21 +23,29 @@ class ProductsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
-  def delete
-
+  def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
 
   def product_params
     params.require(:product).permit(:name, :url)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 
 end
